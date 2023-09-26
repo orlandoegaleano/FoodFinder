a1ddae4c3fcf54c319427a00b8f0c0f6266295aa
orlandoegaleano <orlandoegaleano@gmail.com>
2023-09-26 10:48:16 -0500

    Lecture 9/26
    Started up initial project files, briefly went over Yelp API key access and get requests.

--- App.js
+++ App.js
@@ -18,3 +18,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+//API KEY: 4hxfkQDbLK8Yv7iBTDYqDZQCfA16qyXfHiKnL05lDjA447JiQDDA-_0vQW9oBnLxFq5tDp0c9iGy3ZD-b_HRG7IKbgE0M2ml4KSSOMc0T3UROhM0pkjF8CgIifsSZXYx
+//GET https://api.yelp.com/v3/businesses/search
